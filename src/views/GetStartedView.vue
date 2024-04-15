da1bd9891730067e3ef636b640a90db537ef5bc7
Carolina Belen Maio <cmaio@MacBook-Air-de-Carolina.local>
2024-04-15 19:45:39 -0300

    first commit

---
+++ src/views/GetStartedView.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <main class="main">
+    <div class="main-text">
+      <p class="bold-text">world</p>
+      <p>travel</p>
+    </div>
+    <div class="legend">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+    </div>
+    <button class="button dark">Ingresar</button>
+    <button class="button light">Registrarse</button>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/css/_getStarted.scss';
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="register">
+    <h1>This is an register page</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
